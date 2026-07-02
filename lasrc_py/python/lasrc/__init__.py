4a7357e69aaa5b0dc0840d06c7d2e49e26eb3603
Chris Holden <ceholden@gmail.com>
2026-07-02 15:29:46 -0400

    fix: define/export fill value and write as NDV into outputs

--- lasrc_py/python/lasrc/__init__.py
+++ lasrc_py/python/lasrc/__init__.py
@@ -1,4 +1,4 @@
-from lasrc.lasrc import __version__, PyLookupTables, PyAuxiliaryData, process_surface_reflectance, process_sentinel_surface_reflectance
+from lasrc.lasrc import __version__, PyLookupTables, PyAuxiliaryData, process_surface_reflectance, process_sentinel_surface_reflectance, SR_FILL_VALUE
 from lasrc.pipeline import AuxFilePaths
 
 __all__ = [
@@ -7,5 +7,6 @@ __all__ = [
     "PyAuxiliaryData",
     "process_surface_reflectance",
     "process_sentinel_surface_reflectance",
+    "SR_FILL_VALUE",
     "AuxFilePaths",
 ]
